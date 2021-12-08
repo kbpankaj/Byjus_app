80b48cbef10aff82f28d344154ddcd8984cc76ab
pankaj <kbpankaj45@gmail.com>
2021-12-08 22:49:04 +0530

    added html file

--- Byjusapp/views.py
+++ Byjusapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# from django import render
+
+def html(request):
+    return render(request,"html.html")
 
 # Create your views here.
--- ownproject/settings.py
+++ ownproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Byjusapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ownproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'Templet')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
